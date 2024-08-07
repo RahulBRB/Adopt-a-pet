367d8da5d61f4046198047977ffbc3fbec9a9b2e
Ryzn <rahulblue17@gmail.com>
2024-08-07 17:31:56 +0530

    Uploading the project directory to github repo

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const { worker } = require('./mocks/browser');
+worker.start();
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+
+reportWebVitals();
